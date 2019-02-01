0370994a2ba5a4931fd579e27f37b0643d7aad2a
Stefan <shodgeskluck@gmail.com>
2019-02-01 11:29:56 -0500

    Added toggle for mobile dropdown menu

--- js/Topnav.js
+++ js/Topnav.js
@@ -2,7 +2,7 @@
 document.write( '<div class=\"topnav\" id=\"myTopnav\">\n' );
 document.write( '    <a href=\"./#home\">SHK</a>\n' );
 document.write( '    <div class=\"topnav-menu\">\n' );
-document.write( '        <button id=\"menu-icon\"><i class=\"fa fa-bars\"></i></button>\n' );
+document.write( '        <button class=\"menu-icon\"><i class=\"fa fa-bars\"></i></button>\n' );
 document.write( '      <div class=\"topnav-sections\">\n' );
 document.write( '        <a href=\"./#portfolio\">Portfolio</a>\n' );
 document.write( '        <a href=\"./#about-me\">About</a>\n' );
@@ -11,4 +11,14 @@ document.write( '      </div>\n' );
 document.write( '    </div>\n' );
 document.write( '  </div>' );
 
-//Changing class based on screen size
+//Toggle menu display on small screens
+function dropdownToggle() {
+    var dropdown = document.getElementsByClassName('topnav-sections')[0];
+    dropdown.classList.toggle('topnav-sections_ToggleShow')
+
+    var icon = document.getElementsByClassName('menu-icon')[0];
+    icon.classList.toggle('menu-icon_Toggle')
+}
+
+document.querySelector('.menu-icon')
+    .addEventListener('click', dropdownToggle);
