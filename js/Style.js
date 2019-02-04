e413be066c414ba87eea78a50351ea027f4a4ac2
Stefan <shodgeskluck@gmail.com>
2019-02-04 15:37:09 -0500

    Created Sass stylesheet, nested selectors

--- js/Style.js
+++ js/Style.js
@@ -1,12 +1,12 @@
 
+//Google Fonts
+document.write('<link href="https://fonts.googleapis.com/css?family=Heebo" rel="stylesheet">')
+
 //Main stylesheet
-document.write('<link rel="stylesheet" type="text/css" href="./sass/styles.css">')
+document.write('<link rel="stylesheet" type="text/css" href="./styles/styles.css">')
 
 //Font Awesome
 document.write('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">')
-
-//Google Fonts
-document.write('<link href="https://fonts.googleapis.com/css?family=Heebo" rel="stylesheet">')
 
 //Favicon
 document.write('<link rel="icon" href="shk-favicon.ico">')
